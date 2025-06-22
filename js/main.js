8400d3e8656c500220e880715b810743deb1861f
Sadri199 <adrsa_15@hotmail.com>
2025-06-22 16:21:30 -0300

    Advancing through the functions

--- js/main.js
+++ js/main.js
@@ -9,11 +9,13 @@ let playerStatus = [
     ["Name", "Hero"]
 ]
 
-const atkCounter = 3
-const defCounter = 0
+let atkCounter = 0
+let defCounter = 0
 
 // Enemy Data
 const enemies = ["Goblin", "Bear", "Dragon"]
+
+const instanceEnemy = []
 
 const goblinStats = [
     ["HP", 25],
@@ -36,73 +38,129 @@ const dragonStats = [
 
 const itemStatDatabase =[
     ["short sword", 10],
-    ["banana", 5]
+    ["banana", 5],
+    ["gattling gun", 999]
 ]
 // Functions
-function statCheck () {
-    console.log("Here are your Stats:\n")
-    console.log(playerStatus.join(" - "))
+const nameEdit = (name) => {
+    if (name != "" && name != null){
+        playerStatus.splice(3,1,["Name",name])
+        console.log("Your name will be" + " " + playerStatus[3][1])
+    }
+    else{
+        console.log("Your name will be" + " " + playerStatus[3][1])
+    }
 }
 
-function inventoryCheck (){
+function statCheck () { //Working
+    console.log("Here are your Stats:\n")
+    for (let playerStat of playerStatus){
+        console.log(playerStat.join(" - "))
+    }
+}
+
+function inventoryCheck (){ //Working
     console.log("Here is your Inventory:\n")
     console.log(inventory.join(" - "))
 }
 
-const equipAtk = (item) => {
-    let validate = itemStatDatabase.includes(item) //For of si no no está haciendo nada
-    let index = itemStatDatabase.indexOf(item)
-    
-    if (validate){
-        let sumAtk = playerStatus[1][1] + itemStatDatabase[index][1]
-        console.log(sumAtk)
-    }
-    else{
-        console.log("equipATK no está funcionando che")
+const equipAtk = (item) => { //Working
+    for (let [name,stat] of itemStatDatabase){
+        let validate = name.includes(item)
+
+        if (validate){
+            let sumAtk = playerStatus[1][1] + stat
+            console.log(sumAtk)
+            playerStatus.splice(1,1,["ATK",sumAtk])
+            console.log(playerStatus)
+        }
     }
 }
 
-function equip (item){
+function equip (item){ //Working
     let exist = inventory.indexOf(item)
 
     if (exist != -1){
         inventory.splice(exist,1)
         equippedItem.push(item)
-        console.log(`You removed ${item} from inventory! \nYou added ${item} to your hand!`)
+        console.log(`You removed ${item} from inventory! \nYou added ${item} to your hand!\n`)
         equipAtk(item)
     }
     else{
-        console.log("You don't have that item in your inventory.")
+        console.log("You don't have that item in your inventory.\n")
     }
 }
 
-const critAtk = () => {
+const critAtk = () => { //Working
+    atkCounter ++
+
     if (atkCounter == 2){
         let currentAtk = playerStatus[1][1] * 1.5
+        console.log("Your Attack rises! \n")
         return currentAtk
     }
     else if (atkCounter == 3){
         let currentAtk = playerStatus[1][1] * 2
+        console.log("Your Attack reaches its limits! \n")
+        atkCounter = 0
         return currentAtk
     }
     else{
         let currentAtk = playerStatus[1][1]
+        console.log("Your Attack stays the same. \n")
         return currentAtk
     }
 }
 
+const critDef = () => { //Working
+    defCounter ++
+
+    if (defCounter == 2){
+        let currentDef = playerStatus[2][1] * 1.5
+        console.log("Your Defense rises! \n")
+        return currentDef
+    }
+    else if (defCounter == 3){
+        let currentDef = playerStatus[2][1] * 2
+        console.log("Your Defense reaches its limits! \n")
+        return currentDef
+    }
+    else{
+        let currentDef = playerStatus[2][1]
+        console.log("Your Defense stays the same. \n")
+        return currentDef
+    }
+}
+
+const attackCalc = (enemy) =>{ //Making
+    instanceEnemy = enemy
+    //Switch para distinguir tipo de enemigo y clonar array a instance enemy
 
 
+    critAtk()
+    let atkDifference = playerStatus[1][1] - enemy[2][1]
+    let hpRemain = atkDifference - enemy[0][1]
+
+}
 //loops and Calls
 
 
 //Testing
 
-//inventoryCheck()
+//nameEdit(prompt('Please type your name or press "Ok" to continue with the default value: \n'))
+
+// equip(prompt("Grab an item from your inventory and equip it to your hand!\n").toLowerCase())
+// console.log(`This is your current equipped item: ${equippedItem}\n`)
+// statCheck()
+// inventoryCheck()
+
 //critAtk()
-equip(prompt("Grab an item from your inventory and equip it to your hand!").toLowerCase())
-console.log(equippedItem)
-statCheck()
+// critDef()
+
+
 // let prueba = playerStatus[1][1] + itemStatDatabase[0][1]
 // playerStatus.splice(1,1,["ATK",prueba])
 // console.log(playerStatus)
+// let prueba = prompt("Que tipo de dato devuelve un prompt vacio?")
+// console.log(prueba)
+// console.log (typeof prueba)
